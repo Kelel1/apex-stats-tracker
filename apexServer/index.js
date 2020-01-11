e41ceaa192a9205bf7a86c9928e56f1e2897599c
Kern Elder <kelel1@yahoo.com>
2020-01-11 16:36:25 -0500

    add starter server code

---
+++ apexServer/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app     = express()
+
+app.get('/', (req, res) => {
+
+  res.send('<h1>Hello Kern</h1>')
+
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log( `Server running on port ${PORT}`)
+})
