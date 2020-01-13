8047bcc00d495834339e8478f2450f5f8e4b493a
Kern Elder <kelel1@yahoo.com>
2020-01-13 17:55:18 -0500

    add background images

--- apex-view/src/index.js
+++ apex-view/src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Background from './images/bangalore.png'
+import Logo from './images/apex.png'
 
+const bangaloreStyle = {
+  position: "fixed",
+  padding: 0,
+  margin: 0,
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundImage: `url(${Background})`  
+}
+const apexLogo = {
+  position: "fixed",
+  opacity: "0.3",
+  top: 0,
+  left: 0,
+  width: "20%",
+  height: "27%",
+  backgroundImage: `url(${Logo})`  
+}
 const App = () => (
   <div>
-    <p>Hello world</p>
+    <section style={ bangaloreStyle }></section>
+    <section style={ apexLogo }></section>    
+    
   </div>
 )
 
